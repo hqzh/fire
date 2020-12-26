d7d59003aa0dbb69744749159a7db330e00a1c88
zhuhq <616928019@qq.com>
2020-12-26 17:51:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="root" id="root"></div>
+</template>
+
+<script>
+import AMap from 'AMap';
+export default {
+    mounted() {
+        this.init();
+    },
+    methods: {
+        init() {
+            new AMap.Map('root', {
+                center: [102.712251, 25.040609],
+                resizeEnable: true,
+                zoom: 10,
+            });
+        },
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.root {
+    height: 100%;
+    width: 100%;
+}
+</style>
