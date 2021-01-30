461f8ace783ae71ddf4856aef13882125d35f3e4
zhuhq <616928019@qq.com>
2021-01-30 23:58:02 +0800

    标记交互

--- src/plugins/ant-design-vue.js
+++ src/plugins/ant-design-vue.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import { Input, Button } from 'ant-design-vue'
 import { LocaleProvider } from 'ant-design-vue'
 
+
 Vue.component(LocaleProvider.name, LocaleProvider)
 Vue.component(Input.Search.name, Input.Search)
 Vue.component(Button.name, Button)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,34 @@
 <template>
     <div id="x6">
-        <div id="x6-menu"></div>
+        <!-- 战斗人员   消防栓  消防通道 -->
+        <div id="x6-menu">
+            <div
+                class="menu-item"
+                v-for="item in list"
+                :key="item.icon"
+                @click="handleClick(item.icon)"
+            >
+                <icon-font :type="item.icon" class="icon-fire" />
+                <span>{{ item.text }}</span>
+                <icon-font type="iconadd" class="icon-fire"></icon-font>
+            </div>
+        </div>
         <div id="x6-container"></div>
     </div>
 </template>
 
 <script>
 import { Graph } from "@antv/x6";
+import { Icon } from "ant-design-vue";
+const IconFont = Icon.createFromIconfontCN({
+    scriptUrl: "//at.alicdn.com/t/font_2358004_i42xq7epmgk.js",
+});
 Graph.registerNode(
     "fireman",
     {
         attrs: {
             image: {
+                event: "cell:contextmenu",
                 "xlink:href": "/static/fireman.png",
                 width: 64,
                 height: 64,
@@ -31,6 +48,7 @@ Graph.registerNode(
     {
         attrs: {
             image: {
+                event: "cell:contextmenu",
                 "xlink:href": "/static/hydrant.png",
                 width: 40,
                 height: 60,
@@ -46,16 +64,32 @@ Graph.registerNode(
     true
 );
 export default {
+    components: {
+        IconFont,
+    },
     data() {
         return {
             graph: null,
+            list: [
+                {
+                    text: "战斗人员",
+                    icon: "iconfireman",
+                },
+                {
+                    text: "消防栓",
+                    icon: "iconhydrant",
+                },
+                {
+                    text: "安全通道",
+                    icon: "iconexit",
+                },
+            ],
         };
     },
     mounted() {
         const width = document.getElementById("x6-container").scrollWidth;
         const height =
             document.getElementById("x6-container").scrollHeight || 600;
-        console.log(height);
         this.graph = new Graph({
             container: document.getElementById("x6-container"),
             width,
@@ -67,38 +101,60 @@ export default {
                 repeat: "no-repeat", // https://developer.mozilla.org/en-US/docs/Web/CSS/background-repeat
             },
         });
-        this.graph.addNode({
-            x: 200,
-            y: 160,
-            shape: "fireman",
+        this.graph.on("cell:contextmenu", ({ view, e }) => {
+            if (e.type === "contextmenu") {
+                e.stopPropagation();
+                view.cell.remove();
+            }
         });
-        this.graph.addNode({
-            x: 400,
-            y: 460,
-            shape: "hydrant",
-        });
-        this.graph.addEdge({
-            source: { x: 40, y: 40 },
-            target: { x: 380, y: 40 },
-            vertices: [
-                { x: 40, y: 80 },
-                { x: 200, y: 80 },
-                { x: 200, y: 40 },
-            ],
-            attrs: {
-                line: {
-                    stroke: "#009944",
-                    strokeWidth: 6,
-                    targetMarker: "classic",
-                },
-            },
-            tools: {
-                name: "vertices",
-                args: {
-                    attrs: { fill: "#f90c2d" },
-                },
-            },
-        });
+    },
+    methods: {
+        handleClick(icon) {
+            console.log(icon);
+            switch (icon) {
+                case "iconfireman":
+                    this.graph.addNode({
+                        x: 500,
+                        y: 400,
+                        shape: "fireman",
+                    });
+                    break;
+                case "iconhydrant":
+                    this.graph.addNode({
+                        x: 500,
+                        y: 400,
+                        shape: "hydrant",
+                    });
+                    break;
+                case "iconexit":
+                    this.graph.addEdge({
+                        source: { x: 400, y: 400 },
+                        target: { x: 800, y: 500 },
+                        vertices: [
+                            { x: 400, y: 300 },
+                            { x: 500, y: 300 },
+                            { x: 500, y: 500 },
+                        ],
+                        attrs: {
+                            line: {
+                                event: "edge:contextmenu",
+                                stroke: "#009944",
+                                strokeWidth: 6,
+                                targetMarker: "classic",
+                            },
+                        },
+                        tools: {
+                            name: "vertices",
+                            args: {
+                                attrs: { fill: "#f90c2d" },
+                            },
+                        },
+                    });
+                    break;
+                default:
+                    break;
+            }
+        },
     },
 };
 </script>
@@ -110,9 +166,20 @@ export default {
 }
 #x6-menu {
     width: 240px;
+    padding-top: 24px;
 }
 #x6-container {
     flex: auto;
     width: 0;
+}
+.icon-fire {
+    font-size: 40px;
+}
+.menu-item {
+    cursor: pointer;
+    padding: 16px;
+    font-size: 24px;
+    font-weight: bold;
+    justify-content: space-between;
 }
 </style>
