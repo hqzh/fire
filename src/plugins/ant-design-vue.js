5e194459f33e1d798c7542677936a080076218fb
hqzh <hqzhhm@gmail.com>
2021-02-11 11:24:56 +0800

    弹窗全屏

--- src/plugins/ant-design-vue.js
+++ src/plugins/ant-design-vue.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import { Input, Button, Modal } from 'ant-design-vue'
+import { Input, Button,Drawer } from 'ant-design-vue'
 import { LocaleProvider } from 'ant-design-vue'
-Modal.install(Vue)
+Drawer.install(Vue)
 
 Vue.component(LocaleProvider.name, LocaleProvider)
 Vue.component(Input.Search.name, Input.Search)
 Vue.component(Button.name, Button)
-Vue.component(Modal.name, Modal)
+Vue.component(Drawer.name, Drawer)
