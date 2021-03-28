1ce3573984bb6b3edeed2747da480e7ddc1eb176
zhuhq <616928019@qq.com>
2021-03-28 16:54:14 +0800

    feat: 进攻路线和供水路线

--- src/plugins/ant-design-vue.js
+++ src/plugins/ant-design-vue.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Input, Button, Drawer, Card ,Select} from 'ant-design-vue'
+import { Input, Button, Drawer, Card ,Select,Checkbox} from 'ant-design-vue'
 import { LocaleProvider } from 'ant-design-vue'
 Drawer.install(Vue)
 Vue.component(LocaleProvider.name, LocaleProvider)
@@ -8,4 +8,5 @@ Vue.component(Button.name, Button)
 Vue.component(Drawer.name, Drawer)
 Vue.component(Card.name, Card)
 Vue.component(Select.name, Select)
+Vue.component(Checkbox.name, Checkbox)
 Vue.component(Select.Option.name, Select.Option)
