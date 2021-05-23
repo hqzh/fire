fe728509e424d6da5455fcc1a8e15436f5e57f53
zhuhq <616928019@qq.com>
2021-05-23 20:40:15 +0800

    性能优化

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
-    configureWebpack: {
-        externals: {
+    configureWebpack: config => {
+        config.externals = {
             'AMap': 'AMap', // 高德地图配置
+        }
+        if (process.env.NODE_ENV === 'production') {
+            const plugins = [
+                new CompressionPlugin({
+                    algorithm: 'gzip',
+                    test: /\.(js|css|json|txt|html|ico|svg|obj|mtl)$/,// 匹配文件名
+                    threshold: 10240, // 对超过10k的数据压缩
+                    deleteOriginalAssets: true, // 不删除源文件
+                    minRatio: 0.8 // 压缩比
+                })
+            ]
+            config.plugins = [...config.plugins, ...plugins];
         }
     }
 }
